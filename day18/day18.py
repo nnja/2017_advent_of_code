13ba019d5fe22a01dc8ab9a072375eaa2bb48731
Nina Zakharenko <nzakharenko@gmail.com>
2017-12-18 23:16:45 -0800

    Day 18 - Part 2

--- day18/day18.py
+++ day18/day18.py
@@ -1,56 +1,63 @@
-# ans day 1: 3423
+# ans part 1: 3423
+# ans part 2: 7493
+
 from collections import defaultdict
 
 
-registers = defaultdict(int)
-
-
-def day18(lines):
+def day18(instructions, pid):
+    registers = defaultdict(int)
+    registers['p'] = pid
     pos = 0
-    last_freq = None
-    command = None
+    send_queue = []
 
-    while pos < len(lines):
-        line = lines[pos]
-        line_input = line.split()
+    while pos < len(instructions):
+        command, arg1, *opt_arg = instructions[pos].strip().split()
+        if opt_arg:
+            val = opt_arg[0]
+            arg2 = registers[val] if val in registers else int(val)
 
-        command = line_input[0]
-        first_op = line_input[1]
-        second_op = line_input[2] if len(line_input) == 3 else None
-
+        if command == 'add':
+            registers[arg1] = registers[arg1] + arg2
+        if command == 'mul':
+            registers[arg1] = registers[arg1] * arg2
+        if command == 'mod':
+            registers[arg1] = registers[arg1] % arg2
+        if command == 'set':
+            registers[arg1] = arg2
         if command == 'snd':
-            last_freq = registers[first_op]
-        elif command == 'rcv':
-            if registers[first_op] != 0:
-                return last_freq
-        elif command == 'jgz':
-            if registers[first_op] > 0:
-                step = get_value(second_op)
-                pos += step
+            send_queue.append(registers[arg1])
+        if command == 'rcv':
+            registers[arg1] = yield send_queue
+            send_queue = []
+        if command == 'jgz':
+            if registers[arg1] if arg1 in registers else int(arg1) > 0:
+                pos += arg2
                 continue
-        else:
-            do_command(command, first_op, second_op)
 
         pos += 1
 
 
-def get_value(second_op):
-    return (int(second_op)
-            if second_op not in registers
-            else registers[second_op])
+instructions = open('input.txt').readlines()
 
+p0 = day18(instructions, pid=0)
+p1 = day18(instructions, pid=1)
 
-def do_command(command, arg1, arg2):
-    value = get_value(arg2)
-    if command == 'add':
-        registers[arg1] = registers[arg1] + value
-    if command == 'mul':
-        registers[arg1] = registers[arg1] * value
-    if command == 'mod':
-        registers[arg1] = registers[arg1] % value
-    if command == 'set':
-        registers[arg1] = value
+send_to_1, send_to_0 = next(p0), next(p1)
+answer = 0
 
+while True:
+    while send_to_0:
+        yielded_by_0 = p0.send(send_to_0.pop(0))
+        if yielded_by_0:
+            send_to_1 += yielded_by_0
+    while send_to_1:
+        yielded_by_1 = p1.send(send_to_1.pop(0))
+        if yielded_by_1:
+            send_to_0 += yielded_by_1
+            answer += len(yielded_by_1)
 
-file_input = open('input.txt').read()
-print day18(file_input.splitlines())
+    if not (send_to_0 or send_to_1):
+        break
+
+print('Part 2: %s' % answer)
+
