366fe443894b4a7de67392d098c3f4a12eb543dd
Nina Zakharenko <nzakharenko@gmail.com>
2017-12-20 22:13:03 -0800

    Day 16 Part 2

--- day16/day16.py
+++ day16/day16.py
@@ -1,3 +1,6 @@
+# Part1: ehdpincaogkblmfj
+# Part2: bpcekomfgjdlinha
+
 programs = [chr(c) for c in range(ord('a'), ord('a') + 16)]
 
 def spin(n):
@@ -9,9 +12,7 @@ def exchange(pos_a, pos_b):
     pos_a = int(pos_a)
     pos_b = int(pos_b)
 
-    keep = programs[pos_a]
-    programs[pos_a] = programs[pos_b]
-    programs[pos_b] = keep
+    programs[pos_a], programs[pos_b] = programs[pos_b], programs[pos_a]
 
 def partner(a, b):
     pos_a = programs.index(a)
@@ -24,23 +25,19 @@ operations = {
     'p': lambda x, y: partner(x, y),
 }
 
-def day16(input):
-    for op in input.split(','):
-        command = op[0]
-        a, *b = op[1:].split('/')
-        operations[command](a, b[0] if b else None)
+ops = open('input.txt').read().split(',')
+def day16():
+    seen = []
+    for i in range(1000000000):
+        target = ''.join(programs)
+        if target in seen:
+            print(seen[1000000000 % i])
+            return
+        seen.append(target)
 
-    print('Part1: ' + ''.join(programs))
+        for op in ops:
+            command = op[0]
+            a, *b = op[1:].split('/')
+            operations[command](a, b[0] if b else None)
 
-
-# test_input = 's1,x3/4,pe/b'
-# test_output = 'baedc'
-# day16(test_input)
-day16(open('input.txt').read())
-
-# input = open('input.txt').read()
-# for i in range(10000):
-#     if i % 10000000 == 0:
-#         print('.')
-#     foo = day16(input)
-# print('Part2: %s' % foo)
+day16()
